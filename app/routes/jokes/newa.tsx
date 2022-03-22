4f70e79e2b9f8b53d88710b9fcbd5878fdc6b1cc
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2022-03-21 19:20:49 -0600

    Gezz

--- app/routes/jokes/newa.tsx
+++ app/routes/jokes/newa.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/provider";
 import { Container, Input, Text, Box } from "@chakra-ui/react";
+import { Form } from "remix";
 function newa() {
   return (
     <ChakraProvider>
       <Container>
-        <Text>Input</Text>
-        <Input title="Input"></Input>
-        <Box my={"1rem"}>
-          <Text my={"10px"}>Otro input</Text>
-          <Input title="Input"></Input>
-        </Box>
+        <Form method="post" action="/events">
+          <Box my={"1rem"}>
+            <Text my={"10px"}>Otro input</Text>
+            <Input title="Input" type={"number"}></Input>
+          </Box>
+        </Form>
       </Container>
     </ChakraProvider>
   );
